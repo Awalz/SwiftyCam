d4e5df54760c3dd6f854b1dca8e94ca198c6d394
erikterwan <erik@erikterwan.com>
2017-02-20 13:50:37 +0100

    Fixed focusRing nil check

--- Source/SwiftyCamViewController.swift
+++ Source/SwiftyCamViewController.swift
@@ -928,7 +928,7 @@ extension SwiftyCamViewController {
 			return
 		}
 		
-		if showFocusRing {
+		if showFocusRing && focusRing == nil {
 			focusRing = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
 			focusRing?.backgroundColor = UIColor(white: 1.0, alpha: 0.3)
 			focusRing?.translatesAutoresizingMaskIntoConstraints = false
