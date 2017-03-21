f3321c0ea63403502037784078600f039cc82ba8
Jon Andersen <jon@andersen.re>
2017-03-20 21:53:33 -0400

    Add Carthage support

---
+++ DemoSwiftyCam/SwiftyCam-iOS/SwiftyCam-iOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyCam-iOS.h
+//  SwiftyCam-iOS
+//
+//  Created by Jon Andersen on 3/20/17.
+//  Copyright © 2017 Cappsule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyCam-iOS.
+FOUNDATION_EXPORT double SwiftyCam_iOSVersionNumber;
+
+//! Project version string for SwiftyCam-iOS.
+FOUNDATION_EXPORT const unsigned char SwiftyCam_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCam_iOS/PublicHeader.h>
+
+
